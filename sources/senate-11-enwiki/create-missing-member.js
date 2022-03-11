5c8885c93423e3148e88ff26b9891075abfc3430
Tony Bowden <tony@tmtm.com>
2022-03-11 11:22:32 +0200

    Senate 11: refresh data

--- sources/senate-11-enwiki/create-missing-member.js
+++ sources/senate-11-enwiki/create-missing-member.js
@@ -1,15 +1,29 @@
-module.exports = (label) => {
+const fs = require('fs');
+let rawmeta = fs.readFileSync('meta.json');
+let meta = JSON.parse(rawmeta);
+
+module.exports = (label,party,gender) => {
+  mem = {
+    value: meta.position,
+    qualifiers: {
+      P2937: meta.term.id,
+      P4100: party,
+    },
+    references: { P854: meta.source }
+  }
+
+  claims = {
+    P31: { value: 'Q5' }, // human
+    P106: { value: 'Q82955' }, // politician
+    P39: mem,
+  }
+  if(gender == 'male')   claims['P21'] = 'Q6581097';
+  if(gender == 'female') claims['P21'] = 'Q6581072';
+
   return {
     type: 'item',
-    labels: {
-      en: label,
-    },
-    descriptions: {
-      en: 'Nigerian politician',
-    },
-    claims: {
-      P31: { value: 'Q5' }, // human
-      P106: { value: 'Q82955' }, // politician
-    }
+    labels: { en: label },
+    descriptions: { en: 'Kenyan politician' },
+    claims: claims,
   }
 }
