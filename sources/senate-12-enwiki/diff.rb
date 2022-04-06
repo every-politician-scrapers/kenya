4086b56a152fdb8ca0c838e4cb0e454d0a67811c
Tony Bowden <tony@tmtm.com>
2022-04-06 11:37:11 +0300

    Switch to nullless diffs

--- sources/senate-12-enwiki/diff.rb
+++ sources/senate-12-enwiki/diff.rb
@@ -3,7 +3,7 @@
 
 require 'every_politician_scraper/comparison'
 
-class Comparison < EveryPoliticianScraper::Comparison
+class Comparison < EveryPoliticianScraper::NulllessComparison
   def columns
     super | [:psid]
   end
