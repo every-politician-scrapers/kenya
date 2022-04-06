1c5f82a8854aa45fc00a220e52792b0a89fa6c42
Tony Bowden <tony@tmtm.com>
2022-04-06 11:24:37 +0300

    make comparisons less verbose

--- sources/representatives-11-ep/compare.sh
+++ sources/representatives-11-ep/compare.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 # curl https://raw.githubusercontent.com/everypolitician/everypolitician-data/master/data/Kenya/Assembly/term-11.csv| qsv select wikidata,name,wikidata_group,group,wikidata_area,area,start_date,end_date,gender | qsv rename item,itemLabel,party,partyLabel,area,areaLabel,startDate,endDate,gender > scraped.csv
 
-wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv sort -s itemLabel,startDate | ifne tee wikidata.csv
+wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv sort -s itemLabel,startDate > wikidata.csv
 bundle exec ruby diff.rb | qsv sort -s itemlabel,startdate | tee diff.csv
 
 cd -
