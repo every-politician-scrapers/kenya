cbfb0aac56337cd407b1e671859e3fce3cca96d8
Tony Bowden <tony@tmtm.com>
2022-06-15 10:34:19 +0300

    update to latest wpoffice

--- sources/minister-education/scraper.rb
+++ sources/minister-education/scraper.rb
@@ -6,42 +6,21 @@ require 'pry'
 
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
-  decorator UnspanAllTables
   decorator WikidataIdsDecorator::Links
 
-  # TODO: make this easier to override
   def holder_entries
     noko.xpath('.//h2[contains(.,"Institutions")]/following::*').remove
     noko.xpath('.//h2[contains(.,"Ministers")]/following::ol//li[a]')
   end
 
-  class Officeholder < OfficeholderBase
-    def raw_combo_dates
-      noko.text.scan(/(\d{4})/).flatten
+  class Officeholder < OfficeholderNonTableBase
+    def name_node
+      noko.css('a').last
     end
 
-    def endDate
-      raw_combo_dates.last
-    end
-
-    def combo_date?
-      true
-    end
-
-    def item
-      name_cell.attr('wikidata')
-    end
-
-    def name
-      nake_cell.text.tidy
-    end
-
-    def name_cell
-      noko.css('a').first
-    end
-
-    def empty?
-      false
+    # TODO: only take consecutive years
+    def combo_date
+      noko.text.scan(/(\d{4})/).flatten.values_at(0, -1)
     end
   end
 end
