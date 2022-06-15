2d27802ce026cbc044132bdb2b98a36392b08a46
Tony Bowden <tony@tmtm.com>
2022-06-15 10:50:24 +0300

    update to latest wpoffice

--- sources/minister-agriculture/scraper.rb
+++ sources/minister-agriculture/scraper.rb
@@ -6,43 +6,26 @@ require 'pry'
 
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
-  decorator UnspanAllTables
   decorator WikidataIdsDecorator::Links
 
-  # TODO: make this easier to override
   def holder_entries
     noko.css('td.navbox-list ul li')
   end
 
-  class Officeholder < OfficeholderBase
+  class Officeholder < OfficeholderNonTableBase
+    def name_node
+      noko.css('a').last
+    end
+
     def raw_combo_date
-      noko.text.scan(/\((.*?)\)/).flatten.last
+      years = noko.text[/\((.*?)\)/, 1]
+      years =~ /^\d{4}$/ ? "#{years} - #{years}" : years
     end
 
     def raw_combo_dates
       ds = super
       ds.last.prepend(ds.first[0...2]) if ds.last.length == 2
       ds
-    end
-
-    def combo_date?
-      true
-    end
-
-    def item
-      name_cell.attr('wikidata')
-    end
-
-    def name
-      nake_cell.text.tidy
-    end
-
-    def name_cell
-      noko.css('a').first
-    end
-
-    def empty?
-      false
     end
   end
 end
